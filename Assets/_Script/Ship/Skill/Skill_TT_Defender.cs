26a5898f772774822a355b873b12deb36c5b34e8
sunjiahaoz <sunjiahaoz@126.com>
2016-04-26 14:15:40 +0800

    防护罩技能

---
+++ Assets/_Script/Ship/Skill/Skill_TT_Defender.cs
@@ -0,0 +1,23 @@
+﻿/*
+Skill_TT_Defender
+By: @sunjiahaoz, 2016-4-26
+
+防护罩技能
+ * 触发型技能
+ * 在飞船所在位置撑起防护罩
+ * 防护罩可以对指定Layer对象造成伤害
+*/
+using UnityEngine;
+using System.Collections;
+
+public class Skill_TT_Defender : PlayerShipSkillTriggerTypeBase {
+    public SkillElem_Defender _prefabDefender;
+
+    protected override void CastSkill()
+    {
+        base.CastSkill();
+        SkillElem_Defender dfder = ObjectPoolController.Instantiate(_prefabDefender.gameObject, transform.position, Quaternion.identity).GetComponent<SkillElem_Defender>();
+        dfder.gameObject.SetActive(true);
+        dfder.InitDefend();
+    }
+}
