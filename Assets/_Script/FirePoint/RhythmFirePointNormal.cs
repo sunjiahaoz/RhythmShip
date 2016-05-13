72559b8df65be5989654e1e48293bcf008cd64ab
sunjiahaoz <sunjiahaoz@126.com>
2016-05-13 14:46:07 +0800

    可记录的eventDelegate

---
+++ Assets/_Script/FirePoint/RhythmFirePointNormal.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using sunjiahaoz;
+
+public class RhythmFirePointNormal : FirePointRhythm {
+    public List<EventDelegate> _lstRhythm;
+
+    public override void Fire()
+    {
+        base.Fire();
+        EventDelegate.Execute(_lstRhythm);        
+    }
+}
